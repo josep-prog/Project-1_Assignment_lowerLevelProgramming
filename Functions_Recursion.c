a9c7a05ec8831f86589454a1084c5ffb1a04be5a
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-02-10 07:35:49 +0200

    Question 3: Functions & Recursion

---
+++ Functions_Recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int n);
+int addNumbers(int a, int b);
+void printHello();
+
+int main() {
+    int number;
+    int sum;
+
+    printHello();
+
+    printf("Enter a positive number to calculate factorial: ");
+    scanf("%d", &number);
+
+    printf("Factorial of %d is %d\n", number, factorial(number));
+
+    sum = addNumbers(number, 10);
+    printf("%d + 10 = %d\n", number, sum);
+
+    return 0;
+}
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
