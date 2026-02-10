2d0b3adabecdacd6cc588da35a8827d3543ca359
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-02-10 08:09:59 +0200

    Functions_Recursion.c

--- Functions_Recursion.c
+++ Functions_Recursion.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 
-int factorial(int n);
-int addNumbers(int a, int b);
+long long factorial(int n);
 
 int main() {
     int number;
-    int sum;
-
     printf("Enter a positive number: ");
     scanf("%d", &number);
 
-    printf("Factorials from 1 to %d:\n", number);
-    for (int i = 1; i <= number; i++) {
-        printf("%d! = %d\n", i, factorial(i));
-    }
+    printf("numbers in %d! = ", number);
+    for (int i = 1; i <= number; i++)
+        printf("%d%s", i, (i != number) ? ", " : "\n");
 
-    sum = addNumbers(number, 10);
-    printf("%d + 10 = %d\n", number, sum);
+    printf("%d! = %lld\n", number, factorial(number));
 
     return 0;
 }
 
-int factorial(int n) {
-    if (n == 0 || n == 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
-
-int addNumbers(int a, int b) {
-    return a + b;
+long long factorial(int n) {
+    return (n <= 1) ? 1 : n * factorial(n - 1);
 }
